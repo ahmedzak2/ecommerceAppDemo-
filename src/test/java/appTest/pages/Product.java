6db760bd645ba453981e207ea3befd01154feb3f
ahmed zakria <48220388+ahmedzak2@users.noreply.github.com>
2024-01-27 22:37:30 +0200

    first test caes

--- src/test/java/appTest/pages/Product.java
+++ src/test/java/appTest/pages/Product.java
@@ -9,8 +9,8 @@ public class Product extends Basepage {
  private By header = By.xpath("//android.view.ViewGroup[@content-desc=\"container header\"]/android.widget.TextView");
 private By filter = By.xpath("//*[contains(@content-desc, 'sort button')]/android.widget.ImageView");
 private By priceHighToLow = By.xpath("//*[contains(@content-desc, 'priceDesc')]/android.widget.TextView[2]\n");
-private By firstHighestItem = By.xpath("(//android.widget.TextView[@content-desc='store item text'])[1]");
- private By secondHighestItem = By.xpath("(//android.widget.TextView[@content-desc='store item text'])[2]");
+ private By firstHighestItem = By.xpath("(//android.view.ViewGroup[@content-desc='store item']//android.widget.ImageView)[1]");
+ private By secondHighestItem =  By.xpath("(//android.view.ViewGroup[@content-desc='store item']//android.widget.ImageView)[2]");
 private AppiumBy addToCart = (AppiumBy) AppiumBy.accessibilityId("Add To Cart button");
 private By addItem = By.xpath("//android.view.ViewGroup[@content-desc=\"counter plus button\"]/android.widget.ImageView");
 private By decraseItem = By.xpath("//android.view.ViewGroup[@content-desc='counter minus button']/android.widget.ImageView");
@@ -18,6 +18,7 @@ private By decraseItem = By.xpath("//android.view.ViewGroup[@content-desc='count
 private By MenuApp = By.xpath(" //android.view.ViewGroup[@content-desc='open menu']/android.widget.ImageView");
 
  private AppiumBy catalogMenu = (AppiumBy) AppiumBy.accessibilityId("open menu");
+ private By cart = By.xpath("//android.view.ViewGroup[@content-desc='cart badge']");
  private By catalog = By.linkText("Catalog");
 public Product(AppiumDriver driver) {
   super(driver);
@@ -32,7 +33,6 @@ findeElment(filter).click();
  }
  public void chooseFormOption() throws InterruptedException {
  findeElment(priceHighToLow).click();
- Thread.sleep(100000);
  }
 
 
@@ -43,5 +43,25 @@ findeElment(filter).click();
  public void setFirstHighestItem(By firstHighestItem) {
   this.firstHighestItem = firstHighestItem;
  }
+
+ public void chooseFirstProduct(){
+ findeElment(firstHighestItem).click();
+ }
+ public void addProductToCart(){
+ findeElment(addToCart).click();
+ }
+public void openMenu(){
+ findeElment(MenuApp).click();
 }
+public void  chooseCatolg(){
+ findeElment(catalogMenu).click();
+}
+public void chooseSecondHightProduct(){
+ findeElment(secondHighestItem).click();
+}
+public void clickOnCart(){
+ findeElment(cart).click();
+}
+}
+
 
--- src/test/java/appTest/testcases/BasicTestCases.java
+++ src/test/java/appTest/testcases/BasicTestCases.java
@@ -10,5 +10,14 @@ public class BasicTestCases extends BaseTest {
         Assert.assertEquals(text, "Products");
         productPage.clickONFilter();
     productPage.chooseFormOption();
+    productPage.chooseFirstProduct();
+    productPage.addProductToCart();
+    Thread.sleep(10000);
+    productPage.openMenu();
+    productPage.chooseCatolg();
+    productPage.chooseSecondHightProduct();
+    productPage.addProductToCart();
+productPage.clickOnCart();
+    Thread.sleep(20000);
     }
 }
